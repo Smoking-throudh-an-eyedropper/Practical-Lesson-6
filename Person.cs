c679277d459df186bb765425c17f9a377df7828e
Smoking-throudh-an-eyedropper <menshagina.a@yandex.ru>
2023-04-03 10:52:12 +0700

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Lesson__6
+{
+    internal class Person
+    {
+        private string _name;
+        private uint _head;
+        private uint _torso;
+        private uint _arms;
+        private uint _legs;
+        public string Name 
+        { 
+            get { return _name; }
+            private set { _name = value; }
+        }
+        public uint Head
+        {
+            get { return _head; }
+            private set { _head = value; }
+        }
+        public uint Torso
+        {
+            get { return _torso; }
+            private set { _torso = value; }
+        }
+        public uint Arms
+        {
+            get { return _arms; }
+            private set { _arms = value; }
+        }
+        public uint Legs
+        {
+            get { return _legs; }
+            private set { _legs = value; }
+        }
+        public Person(string name, uint head, uint torso, uint arms, uint legs)
+        {
+            if (name == null)
+                throw new NullReferenceException("Name");
+            Name = name;
+            Head = head;
+            Torso = torso;
+            Arms = arms;
+            Legs = legs;
+        }
+        public void Read()
+        {
+            Console.WriteLine("Укажите имя человека:");
+            Name = Convert.ToString(Console.Read());
+            Console.WriteLine("Укажите обхват головы человека:");
+            Head = Convert.ToUInt16(Console.Read());
+            Console.WriteLine("Укажите длину тела человека:");
+            Torso = Convert.ToUInt16(Console.Read());
+            Console.WriteLine("Укажите длину рук человека:");
+            Arms = Convert.ToUInt16(Console.Read());
+            Console.WriteLine("Укажите длину ног человека:");
+            Legs = Convert.ToUInt16(Console.Read());
+            Console.WriteLine("");
+        }
+        public void Display()
+        {
+            Console.WriteLine("Person");
+            Console.WriteLine($"Name:{Name}");
+            Console.WriteLine($"Head:{Head}");
+            Console.WriteLine($"Torso:{Torso}");
+            Console.WriteLine($"Arms:{Arms}");
+            Console.WriteLine($"Legs:{Legs}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Practical_Lesson__6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Lesson_6_Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтобы дабавить элемент в список введите [1]");
+            int add = Convert.ToInt16(Console.ReadLine());
+            var ListPerson = new List<Person>
+            {
+            };
+            if (add==1)
+            {
+                ListPerson.Add(new Person(Convert.ToString(Console.ReadLine()), Convert.ToUInt16(Console.Read()), Convert.ToUInt16(Console.Read()), Convert.ToUInt16(Console.Read()), Convert.ToUInt16(Console.Read())));
+                Console.WriteLine("Чтобы закончить добавление элементов в список введите [0]");
+                add = Convert.ToInt16(Console.ReadLine());
+            }
+            else
+            {
+                if (add == 0)
+                {
+                    Console.WriteLine("Добавление элементов в список завершено");
+                }  
+            }
+            var LongPeople = ListPerson.Where(l => l.Arms > 65 && l.Legs == 90)
+                                       .Select(l => l.Name)
+                                       .ToList();
+            LongPeople.ForEach(p => Console.WriteLine(p));
+            var HeadsPeople = ListPerson.Select(h => h.Head)
+                                        .ToList();
+            HeadsPeople.ForEach(h => Console.WriteLine(h));
+        }
+    }
+}
